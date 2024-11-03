f1bd9de143752f30249f95c462e18d9e4f917242
Sergino Delia <sergino.co@gmail.com>
2024-11-03 13:38:39 -0500

    added updates

--- 5-Route.tf
+++ 5-Route.tf
@@ -18,8 +18,7 @@ locals {
       name = name
     }
   }
-
-
+  # iterate over the local.public-azs create a map of the public AZs
   public = {
     for key, value in local.public-azs : key => {
       key   = key
@@ -55,78 +54,9 @@ resource "aws_route_table" "rt" {
   }
 }
 
-# resource "aws_route_table" "private" {
-#   vpc_id = aws_vpc.tg-vpcs["vpc-a-virginia-prod"].id
-
-#   route = [
-#     {
-#       cidr_block                 = "0.0.0.0/0"
-#       nat_gateway_id             = aws_nat_gateway.nat-gw-a-virginia-prod.id
-#       carrier_gateway_id         = ""
-#       destination_prefix_list_id = ""
-#       egress_only_gateway_id     = ""
-#       gateway_id                 = ""
-#       instance_id                = ""
-#       ipv6_cidr_block            = ""
-#       local_gateway_id           = ""
-#       network_interface_id       = ""
-#       transit_gateway_id         = ""
-#       vpc_endpoint_id            = ""
-#       vpc_peering_connection_id  = ""
-#     },
-#   ]
-
-#   tags = {
-#     Name = "private"
-#   }
-# }
-
-# resource "aws_route_table" "public" {
-#   vpc_id = aws_vpc.tg-vpcs["vpc-a-virginia-prod"].id
-
-#   route = [
-#     {
-#       cidr_block                 = "0.0.0.0/0"
-#       gateway_id                 = aws_internet_gateway.igw-a-virginia-prod.id
-#       nat_gateway_id             = ""
-#       carrier_gateway_id         = ""
-#       destination_prefix_list_id = ""
-#       egress_only_gateway_id     = ""
-#       instance_id                = ""
-#       ipv6_cidr_block            = ""
-#       local_gateway_id           = ""
-#       network_interface_id       = ""
-#       transit_gateway_id         = ""
-#       vpc_endpoint_id            = ""
-#       vpc_peering_connection_id  = ""
-#     },
-#   ]
-
-#   tags = {
-#     Name = "public"
-#   }
-# }
-
-# aws_route_table.rt["private"].id
-# aws_route_table.rt["public"].id
-# aws_route_table.rt["${each.key}"].id
-
 # create 2 route table associations one private and one public
 resource "aws_route_table_association" "us-east-1" {
   for_each       = var.route-tables
   subnet_id      = aws_subnet.tg-subnets["${each.value.subnet}"].id
   route_table_id = aws_route_table.rt["${each.key}"].id
 }
-
-# resource "aws_route_table_association" "private-us-east-1" {
-#   for_each       = local.private
-#   subnet_id      = aws_subnet.tg-subnets["subnet-a-virginia-prod-private"].id
-#   route_table_id = aws_route_table.private.id
-# }
-
-# # #public
-# resource "aws_route_table_association" "public-us-east-1" {
-#   for_each       = local.public
-#   subnet_id      = aws_subnet.tg-subnets["subnet-a-virginia-prod-public"].id
-#   route_table_id = aws_route_table.public.id
-# }
